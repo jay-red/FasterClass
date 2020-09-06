e695ec83c8ff5c534aa0d623868053ad270ab839
Jared Flores <jared_flores@cs.ucsb.edu>
2020-09-06 03:25:53 -0700

    added more info

--- docs/lessons/template/js/default.js
+++ docs/lessons/template/js/default.js
@@ -1,6 +1,6 @@
 /* 1:USER:DEFAULT */
 // The line above signifies
-// 1 - the priority
+// 1 - the priority (the lower the number, the higher the priority)
 // USER - this is the file the student will be editing
 // DEFAULT - this file can be used as a default when no code has been created yet
 
