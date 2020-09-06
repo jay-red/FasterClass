9c710db837253f54640c0b0a5777170c9c067835
Jared Flores <jared_flores@cs.ucsb.edu>
2020-09-06 05:21:33 -0700

    added course material

---
+++ docs/lessons/webrtc-intro/js/main.js
@@ -0,0 +1,7 @@
+/* 2 */
+function init_main() {
+	var DIV_CONTENT = document.getElementById( "content" );
+    // Append children to DIV_CONTENT to display elements to the page!
+}
+
+init_main();
---
+++ docs/lessons/ws-intro/js/default.js
@@ -0,0 +1,7 @@
+/* 1:USER:DEFAULT */
+// The line above signifies
+// 1 - the priority (the lower the number, the higher the priority)
+// USER - this is the file the student will be editing
+// DEFAULT - this file can be used as a default when no code has been created yet
+
+// Check out this file at /docs/lessons/template/js/default.js
